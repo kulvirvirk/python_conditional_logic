39b14723300febabe094f3728327e359f5c60495
kulvirvirk <kvirkdev@gmail.com>
2020-02-18 06:31:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# 1. create two int variables and compare them using if keyword 
+
+
+
+
+# 1. create two int variables and compare them using if keyword 
+var1 = input('enter first number:')
+var2 = input('enter second number:')
+
+if var1 > var2:
+    print('First number is greater.')
+elif var2 > var1:
+    print('Second number is greater.')
+else: print('They are equal!')
